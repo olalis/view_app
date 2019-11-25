31464516f5e3eae4f129634393f18c2327b870eb
Student <student@d17-l427-pc-11.ipa.ki.agh.edu.pl>
2019-11-25 09:32:33 +0100

    przyciski

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Clicked {{ title }} times</h1>
+    <button @click="iAmClicked()">More</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    data() {
+      return {title: 0}
+    },
+    methods: {
+      iAmClicked() {
+        this.title = this.title + 1;
+      }
+    }
+  };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
