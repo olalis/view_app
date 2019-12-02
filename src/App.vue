92cbc7dc1fdce5109612581afbeae3499d73cce2
Student <student@d17-l427-pc-11.ipa.ki.agh.edu.pl>
2019-12-02 08:59:46 +0100

    przyciski

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <template>
   <div>
-    <h1>Clicked {{ title }} times</h1>
+    <!---<h1>Clicked {{ title }} times</h1>
     <button @click="iAmClicked()">More</button>
+    <button @click="startover()">Start over</button>--->
+
+    <h1>{{happy ? 'Happy :)' : 'Sad :('}}</h1>
+    <button @click="changemood()">Click me!</button>
+
   </div>
 </template>
 
 <script>
   export default {
     data() {
+      return {
+        happy :false
+      }
       return {title: 0}
     },
     methods: {
       iAmClicked() {
         this.title = this.title + 1;
+      },
+      startover(){
+        this.title = 0;
+      },
+      changemood(){
+        this.happy = !this.happy;
       }
     }
   };
