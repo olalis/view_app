9adef90654c0dc4c75d6506c2281e9793c7625df
Student <student@d17-l427-pc-11.ipa.ki.agh.edu.pl>
2019-12-02 09:29:42 +0100

    zadanie

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,45 @@
 <template>
   <div>
+    <div>
+      <h1>Participants list</h1>
+      <!---<ol v-if="text.length > 0">--->
+        <li>John Doe</li>
+        <li>Robin Hood</li>
+        <li>Chuck Norris</li>
+        <li :key="text" v-for="text in text">{{text}}</li>
+
+      </ol>
+    </div>
+
+    <h3>New participant</h3>
+    <form @submit.prevent="addNewParticipant()">
+      <label>Firstname</label>
+      <input type="text">
+      <label>Lastname</label>
+      <input type="text">
+      <button>Add new participant</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {name:[]};
+    },
+    methods: {
+      addNewParticipant() {
+        this.name.push;
+      }
+    }
+  };
+</script>
+
+
+
+
+<!---<template>
+  <div>
     <ul v-if="numbers.length > 0 && numbers.length <10">
       <li :key="number" v-for="number in numbers">{{ number }}</li>
     </ul>
